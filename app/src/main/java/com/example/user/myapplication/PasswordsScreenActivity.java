6ef53e96c593a0f08889acd149bcd3ec953ed30d
EranSharon <eransharon10@gmail.com>
2017-06-15 00:55:41 +0300

    15.6.17

--- app/src/main/java/com/example/user/myapplication/PasswordsScreenActivity.java
+++ app/src/main/java/com/example/user/myapplication/PasswordsScreenActivity.java
@@ -15,29 +15,44 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import android.content.Intent;
+import android.os.AsyncTask;
 
 public class PasswordsScreenActivity extends AppCompatActivity
 {
 
     //-----------------------------------------------------------------------------
-    private static final String host_name = "10.0.0.2";//ipv4 wifi address
+    private static final String host_name = "10.0.0.7";//ipv4 wifi address
     private static final int port_number = 8080;
     private static final String debug_string = "debug";
     private boolean delete = false;
     private String temp_global_account = "";
-    private String global_message_string = "";
-    int  debug_eran = 0;
+    int debug_eran = 0;
 
-
-
+    //DoInBackround global variables
+    String global_message_string = "";
+    String add_message = "";
+    String delete_message = "";
+    String returnFromServer_global = "";
+    boolean user_sign_in = false;
+    boolean user_add = false;
+    boolean user_delete = false;
+    String sign_in_string;
+    boolean response_finish = false;
     //-----------------------------------------------------------------------------
 
     @Override
@@ -54,8 +69,11 @@ public class PasswordsScreenActivity extends AppCompatActivity
         final ListView accountsListView = (ListView) findViewById(R.id.my_list);
         accountsListView.setAdapter(adapter);
 
-        //--------------------------------------------------------------------------First time run code
-        final String PREFS_NAME = "accountInfo";
+        MyClientTask Task = new MyClientTask(host_name, port_number);
+        Task.execute();
+
+
+        final String PREFS_NAME = "accountInfo"; //the official SharedPreferences of the client side
         final SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         settings.edit().putBoolean("my_first_time", true).commit();//JUST FOR DEBUGING DELETE THIS LINE********************
 
@@ -67,37 +85,43 @@ public class PasswordsScreenActivity extends AppCompatActivity
             //NOW THE server gives a unique ID for identification ********************
             //here we send to the server a userId that is not exist like -999 (because its the first time, the user has no id)
 
-            //String returnFromServer = send_message("100,-999");  //**************we need this
+            //String returnFromServer = send_message("100,-999");  //**************we need this*************************************************
 
-            String returnFromServer = "101,1,facebook,eransharon,123\n" +
-                    "facebook,eransharon,123\n" +
-                    "facebook,eransharon,123\n" +
-                    "facebook,eransharon,123\n" +
-                    "facebook,eransharon,123\n" +
-                    "facebook,eransharon,123"
-                    ;
+            sign_in_string = "100,-999";
+            user_sign_in = true;
+            while(response_finish == false); //waiting for the server to response(from the doInbackRound)
+            response_finish = false;
+            //createAccountsList(returnFromServer_global, adapter);
 
-            String message_parts[] = returnFromServer.split(","); // cheack this
+            String message_parts[] = returnFromServer_global.split(","); // cheack this
 
             //here we save the user ID that will send in every future message
             SharedPreferences.Editor editor = settings.edit();
             editor.putInt("ID",Integer.parseInt(message_parts[1]));//****crashes here
             editor.commit();
-            int id = settings.getInt("ID",-1001);
             editor.putBoolean("my_first_time", false).commit();
-            editor.putBoolean("add", false);
-            boolean nice = settings.getBoolean("add", true);
+            debug_eran = settings.getInt("ID",-1000);
+            //editor.putBoolean("add", false);
+            //boolean nice = settings.getBoolean("add", true);
             editor.commit();
-            createAccountsList(returnFromServer, adapter);
-
         }
         else
         {
             //this line will run at start on every user that already used the app
             //this line returns the AccountsTableString from the server
+            // String returnFromServer = "101,1\n"+    //example of what the server sent
+            //"facebook,eransharon,123";
+
             debug_eran = settings.getInt("ID",-1000);
-            send_message("100," + Integer.toString(settings.getInt("ID",-1000)));
+            //String returnFromServer = send_message("100," + Integer.toString(settings.getInt("ID",-1000)));
+            sign_in_string = "100," + Integer.toString(settings.getInt("ID",-1000));
+            user_sign_in = true;
+            while(response_finish == false); //waiting for the server to response(from the doInbackRound)
+            response_finish = false;
+            createAccountsList(returnFromServer_global, adapter);
+
         }
+
         //-----------------------------------------------------------------------------------
 
 
@@ -106,27 +130,35 @@ public class PasswordsScreenActivity extends AppCompatActivity
         {
             public void onClick(View v)
             {
-                adding(adapter);
-                /*
+                //adding(adapter);
+
                 startActivity(new Intent(PasswordsScreenActivity.this, AddingActivity.class));
 
-                if (settings.getBoolean("add",false))
-                {
-                    MessageBox("fasfas");
-                    SharedPreferences sp = getSharedPreferences("accountInfo", Context.MODE_PRIVATE);
-                    temp_global_account =  sp.getString("account","error");
+                MessageBox("fasfas");
+                temp_global_account =  settings.getString("account","error");
+                //adapter.add(stringToOneAccount(temp_global_account)); //Client side add
+                //adapter.notifyDataSetChanged();
 
-                    adapter.add(stringToOneAccount(temp_global_account)); //Client side add
-                    adapter.notifyDataSetChanged();
-
-                    SharedPreferences.Editor editor = settings.edit();
-                    editor.putBoolean("add", false);
-                    editor.commit();
-                }
-                */
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("add", false);
+                editor.commit();
             }
         });
 
+        Button btn = (Button) findViewById(R.id.button8); //update button click
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                //adding(adapter);
+                adapter.add(stringToOneAccount(settings.getString("account","error"))); //Easy and bad solution ***********************
+                adapter.notifyDataSetChanged();
+                add_message = "200," + Integer.toString(settings.getInt("ID",-1000)) + "," + settings.getString("account", "error");//fix this
+                user_add = true;
+                //send_message("400," + Integer.toString(settings.getInt("ID",-1000)) + "," + adapter.getItem(position));
+            }
+
+        });
 
         //--------------------------------------------------------------------------------Here we click on one of the accounts
         accountsListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
@@ -136,9 +168,9 @@ public class PasswordsScreenActivity extends AppCompatActivity
             {
                 if (delete)
                 {
-
-                    //send_message("400," + Integer.toString(settings.getInt("ID",-1000)) + "," + adapter.getItem(position));
                     //  delete the account from the list view (client side)
+                    delete_message = "400," + Integer.toString(settings.getInt("ID",-1000)) + "," + Integer.toString(position);
+                    user_delete = true;
                     adapter.remove(adapter.getItem(position));
                     adapter.notifyDataSetChanged();
                 }
@@ -152,33 +184,17 @@ public class PasswordsScreenActivity extends AppCompatActivity
         //------------------------------------------------------------------------------------
     }
 
-    public void adding(ArrayAdapter<Account> adapter)//View view)
-    {
-        MessageBox("fasfas");
-        final String PREFS_NAME = "accountInfo";
-        final SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 
-
-
-        Intent intent = new Intent(this,AddingActivity.class);
-        startActivity(intent);
-
-
-        SharedPreferences sp = getSharedPreferences("accountInfo", Context.MODE_PRIVATE);
-        temp_global_account =  sp.getString("account","error");
-
-        //send_message("200," + Integer.toString(settings.getInt("ID",-1000)) + "," + temp_global_account);
-
-        adapter.add(stringToOneAccount(temp_global_account)); //Client side add
-        adapter.notifyDataSetChanged();
-    }
 
     //--------------------------------------------------------------------------------------------------------------------server's-string to accounts list-view parsing
     public void createAccountsList(String fullAccountsStringFromServer, ArrayAdapter<Account> adapter)
     {
-
         String[] accounts = fullAccountsStringFromServer.split("\n"); //separate the string into all the Accounts
-        for (String account:  accounts)
+
+        String[] temp=new String[accounts.length - 1];
+        System.arraycopy(accounts,1,temp,0,accounts.length - 1);
+
+        for (String account:  temp)
         {
             //adds all the accounts into the current listView(accountsListView),(using the adapter of this listView)
             adapter.add(stringToOneAccount(account));
@@ -211,13 +227,163 @@ public class PasswordsScreenActivity extends AppCompatActivity
             Toast.makeText(getApplicationContext(),"You have quit delete mode",Toast.LENGTH_SHORT).show();
     }
 
-    public void MessageBox(String message)
+
+    public class MyClientTask extends AsyncTask<Void, Void, Void>
     {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        TextView txtView;
+        String dstAddress;
+        int dstPort;
+
+        MyClientTask(String addr, int port){
+            dstAddress = addr;
+            dstPort = port;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            //txtView = (TextView)findViewById(R.id.response);
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0)
+        {
+            Socket socket = null;
+            try
+            {
+
+                socket = new Socket(dstAddress, dstPort);
+                int bytesRead = 0;
+                while(bytesRead != -1)
+                {
+                    if (user_sign_in == true)
+                    {
+                        ByteArrayOutputStream bos;
+                        bos = new ByteArrayOutputStream(1024);
+                        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                        //String sendInfo = "101" + makeString(2, add_message.length()) + add_message;
+                        out.write(sign_in_string.getBytes(), 0, sign_in_string.getBytes().length);
+
+                        byte[] buffer = new byte[1024];
+                        InputStream inputStream = socket.getInputStream();
+                        bytesRead = inputStream.read(buffer);
+                        ByteArrayOutputStream response = new ByteArrayOutputStream(1024);
+                        response.write(buffer, 0, bytesRead);
+
+                        returnFromServer_global = response.toString("UTF-8");
+                        response_finish = true;
+                        user_sign_in = false;
+                    }
+                    else if(user_add == true)
+                    {
+
+                        ByteArrayOutputStream bos;
+                        bos = new ByteArrayOutputStream(1024);
+                        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                        //String sendInfo = "101" + makeString(2, add_message.length()) + add_message;
+                        out.write(add_message.getBytes(), 0, add_message.getBytes().length);
+
+                        byte[] buffer = new byte[1024];
+                        InputStream inputStream = socket.getInputStream();
+                        bytesRead = inputStream.read(buffer);
+                        ByteArrayOutputStream response = new ByteArrayOutputStream(1024);
+                        response.write(buffer, 0, bytesRead);
+
+                        returnFromServer_global = response.toString("UTF-8");
+                        user_add = false;
+                    }
+                    else if(user_delete == true)
+                    {
+                        ByteArrayOutputStream bos;
+                        bos = new ByteArrayOutputStream(1024);
+                        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                        //String sendInfo = "101" + makeString(2, delete_message.length()) + delete_message;
+                        out.write(delete_message.getBytes(), 0, delete_message.getBytes().length);
+
+                        byte[] buffer = new byte[1024];
+                        InputStream inputStream = socket.getInputStream();
+                        bytesRead = inputStream.read(buffer);
+                        ByteArrayOutputStream response = new ByteArrayOutputStream(1024);
+                        response.write(buffer, 0, bytesRead);
+
+                        returnFromServer_global = response.toString("UTF-8");
+                        user_delete = false;
+                    }
+                }
+            }
+            catch (UnknownHostException e)
+            {
+                e.printStackTrace();
+                returnFromServer_global = "UnknownHostException: " + e.toString();
+                Log.d("MyApp1", returnFromServer_global);
+            } catch (IOException e) {
+                e.printStackTrace();
+                returnFromServer_global = "IOException: " + e.toString();
+                Log.d("MyApp2", returnFromServer_global);
+            }
+            finally
+            {
+                if(socket != null){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+        /*
+        private String makeString(int numOfBytes, int number)
+        {
+            int length = (int)(Math.log10(number)+1);
+            if(length > numOfBytes)
+                return null;
+
+            String str = "";
+            for(int i = 0; i <  numOfBytes - length; i++)
+                str = str + "0";
+
+            str = str + Integer.toString(number);
+            return str;
+        }
+        */
+        /*
+        private int getTypeMessage(String str)
+        {
+            return Integer.parseInt(str.substring(0,3));
+        }
+
+        private String getStringMessage(String str)
+        {
+            String size = str.substring(3,5);
+            int to = Integer.parseInt(size) + 5;
+            return str.substring(5,to);
+        }
+
+        private String handleGetMessage(String str)
+        {
+            int type = getTypeMessage(str);
+            if(type == 101)
+            {
+                return getStringMessage(str);
+            }
+            else
+                return getStringMessage(str);
+        }
+        */
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+        }
     }
 
 
-
+    /*
     public String send_message(final String message)
     {
         new Thread()
@@ -240,13 +406,12 @@ public class PasswordsScreenActivity extends AppCompatActivity
                     bw.newLine();
                     bw.flush();
 
-
                     //func1(adapter);
                     // Receive Message from server
                     BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     global_message_string = br.readLine(); //******************RETURNS THE ANSWER FROM THE SERVER
                     System.out.println("Message from server:" + global_message_string);
-                    //socket.close();
+                    socket.close();
 
                 }
                 catch (IOException e)
@@ -256,6 +421,34 @@ public class PasswordsScreenActivity extends AppCompatActivity
             }
         }.start();
         return global_message_string;
+    }
+    */
+
+
+    public void adding(ArrayAdapter<Account> adapter)//View view)
+    {
+        MessageBox("fasfas");
+        startActivity(new Intent(PasswordsScreenActivity.this, AddingActivity.class));
+        SharedPreferences sp = getSharedPreferences("accountInfo", Context.MODE_PRIVATE);
+        while(true) //while adding activity finish
+        {
+            if (sp.getBoolean("add",false))
+            {
+                MessageBox("fasfas");
+                temp_global_account =  sp.getString("account","error");
+                adapter.add(stringToOneAccount(temp_global_account)); //Client side add
+                adapter.notifyDataSetChanged();
+                break;
+            }
+        }
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("add", false);
+        editor.commit();
+    }
+
+    public void MessageBox(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
 
